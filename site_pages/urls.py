6fb7733c08f99c319f5e7ba8b203b28d3dbd5cdd
TheBoringDude <iamcoderx@gmail.com>
2021-02-06 22:15:14 +0800

    post-conf

--- site_pages/urls.py
+++ site_pages/urls.py
@@ -1,7 +1,8 @@
 """
 URLS Routing for common static pages.
 """
+from site_pages.views.common import about
 
 from django.urls import path
 
-urlpatterns = [path()]
+urlpatterns = [path("/about", about), path('')]
---
+++ store/urls.py
@@ -0,0 +1,10 @@
+"""
+URLS Routing for main-store related pages.
+"""
+from django.urls import path
+
+from store.views import index
+
+urlpatterns = [
+    path('', index.store),
+]
