73a537d1bc865776adb1f8459d95da0c79e3971e
TheBoringDude <iamcoderx@gmail.com>
2021-04-29 15:31:37 +0800

    up and configure packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
-  purge: [],
+  purge: ['./**/*.html'],
   darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
   theme: {
+    colors: colors,
     fontFamily: {
       sans: ['"Rubik"', "sans-serif"],
     },
